e202e452511eef94ea45f955d5b8425fb6ec6f2b
LeGoopyGuy <hdakota884@gmail.com>
2024-09-03 19:58:40 -0500

    added do dads

--- src/main/java/net/LeGoopyGuy/tutorialmod/TutorialMod.java
+++ src/main/java/net/LeGoopyGuy/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.LeGoopyGuy.tutorialmod;
 
+import net.LeGoopyGuy.tutorialmod.block.ModBlocks;
 import net.LeGoopyGuy.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -37,6 +38,7 @@ public class TutorialMod {
         NeoForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -52,6 +54,12 @@ public class TutorialMod {
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.BISMUTH_BLOCK);
+            event.accept(ModBlocks.BISMUTH_ORE);
         }
     }
 
